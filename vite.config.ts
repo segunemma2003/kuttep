38458d4ad0bdb3235fc9b9f9676de3b933ef466a
segunemma2003 <segunemma2003@gmail.com>
2024-10-11 12:04:56 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,5 @@ export default defineConfig({
   plugins: [react(), nodePolyfills()],
   define: {
     "process.env": {},
-    server: {
-      host: "0.0.0.0", // Make the app accessible on your local network (local IP)
-      port: 3111,      // Optional: Set your preferred port
-    },
   },
 });
