8c81d2e2ca2fa51d11c287a21f8d4662aea57fd9
segunemma2003 <segunemma2003@gmail.com>
2024-10-16 11:32:36 +0100

    a

--- src/web3/AppKitProvider.jsx
+++ src/web3/AppKitProvider.jsx
@@ -25,7 +25,7 @@ const metadata = {
 
 // Define the custom networ
 
-const networks =  [mainnet]
+const networks =  [mainnet, sepolia]
 
 // 3. Create Wagmi Adapter
 const wagmiAdapter = new WagmiAdapter({
